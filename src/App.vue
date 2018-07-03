bfcb2c7e0da8e6ad0ce8ee3ec990fa1301832a65
MaTiUs77 <matius77@gmail.com>
2018-07-03 11:36:18 -0300

    Base para PWA Vuetify

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      persistent
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      enable-resize-watcher
+      fixed
+      app
+    >
+      <!-- MENU DE NAVEGACION !-->
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+      <!-- FIN MENU DE NAVEGACION !-->
+    </v-navigation-drawer>
+    <v-toolbar
+      app
+      color="primary"
+      dark
+      :clipped-left="clipped"
+    >
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+        <v-icon>menu</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <!-- CONTENIDO DE NAVEGACION !-->
+    <v-content>
+      <router-view/>
+    </v-content>
+    <!-- FIN CONTENIDO DE NAVEGACION !-->
+
+    <!-- MENU DERECHO !-->
+    <v-navigation-drawer
+      temporary
+      :right="right"
+      v-model="rightDrawer"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile @click="right = !right">
+          <v-list-tile-action>
+            <v-icon>compare_arrows</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- FIN MENU DERECHO !-->
+
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2018</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: true,
+      fixed: false,
+      items: [{
+        icon: 'home',
+        title: 'Inicio'
+      }],
+      right: true,
+      rightDrawer: false,
+      title: 'Siep'
+    }
+  },
+  name: 'App'
+}
+</script>
